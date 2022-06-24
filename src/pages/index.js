dc240c57ad6af5a9b07a9811574392d8d1740ad8
Alex <alexdefaro@gmail.com>
2022-06-24 21:52:44 +0100

    Updating home page message

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <div>
-      Home
-    </div>
-  )
+    return (
+        <div>
+            <h1>Home Page</h1>            
+        </div>
+    )
 }
