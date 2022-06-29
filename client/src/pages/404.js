a47ccf62a42a8a2bee4263430afbdd3a0208f4c3
Alex <alexdefaro@gmail.com>
2022-06-30 00:54:08 +0100

    Fixing eslinst issues

--- client/src/pages/404.js
+++ client/src/pages/404.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react"
 import Router from "next/router";
 
 function PageNotFound() {
@@ -9,7 +9,7 @@ function PageNotFound() {
                     <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
                         <span className="sr-only">Error</span>404
                     </h2>
-                    <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
+                    <p className="text-2xl font-semibold md:text-3xl">Sorry, page not found.</p>
                     <p className="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
                     <a rel="noopener noreferrer" href="#" onClick={() => Router.push("/")}
                         className="px-8 py-3 font-semibold rounded text-white  bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
